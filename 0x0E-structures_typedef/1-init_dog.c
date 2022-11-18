4f9254282964efddfe32a549a8913fc175266757
1appiah <solojappiahgideon@gmail.com>
2022-11-18 04:01:24 +0000

    struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - check the code
+ * @name: dog's name
+ * @d: the dog object
+ * @age: the dog's age
+ * @owner: the dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
